bd93e932b3cd3a4e3f7c1c5eabe471f11aba9df6
Na Dai <daina.lehigh207@gmail.com>
2023-03-20 15:36:03 -0400

    Reorganize data gen and cotrain models.

---
+++ src/data_gen/generate_curve_input_conc_output.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+from typing import Dict, Optional, Text
+
+def decode(serialized_example):
+  # Decode examples stored in TFRecord
+  # NOTE: make sure to specify the correct dimensions for the images
+  feature_tensors = tf.io.parse_single_example(
+      serialized_example,
+      features={
+          'feature/image/avg': tf.io.FixedLenFeature([160, 160, 3], tf.float32),
+          'feature/image/diff': tf.io.FixedLenFeature([160, 160, 3], tf.float32),
+          'feature/image/derivative_1': tf.io.FixedLenFeature([160, 160, 3], tf.float32),
+          'feature/image/derivative_2': tf.io.FixedLenFeature([160, 160, 3], tf.float32),
+          'feature/image/derivative_3': tf.io.FixedLenFeature([160, 160, 3], tf.float32),
+          'feature/image/derivative_4': tf.io.FixedLenFeature([160, 160, 3], tf.float32),
+          'feature/image/derivative_5': tf.io.FixedLenFeature([160, 160, 3], tf.float32),
+          'label/5HT': tf.io.FixedLenFeature([1], tf.float32),
+          'label/ME': tf.io.FixedLenFeature([1], tf.float32),
+          'label/DA': tf.io.FixedLenFeature([1], tf.float32),
+          'label/NE': tf.io.FixedLenFeature([1], tf.float32), })
+
+  # NOTE: No need to cast these features, as they are already `tf.float32` values.
+  return feature_tensors
+
+def separate_features_and_labels(features: Dict) -> Dict:
+  def _get_features_with_prefix(prefix: Text):
+    filtered_features = {}
+    for key in features:
+      if key.startswith(prefix):
+        filtered_features[key] = features[key]
+    return filtered_features
+  return tf.keras.applications.mobilenet_v2.preprocess_input(features['feature/image/avg']), tf.concat(axis=-1,values=[features[label_columns[0]], features[label_columns[1]], features[label_columns[2]], features[label_columns[3]]])
+
+def load_dataset(filename_pattern: Text, 
+                 feature_params: Dict, 
+                 batch_size: int, 
+                 prefetch_size: int, 
+                 repeat: Optional[int] = None):
+  filenames = [filename_pattern] if '*' not in filename_pattern else glob.glob(filename_pattern)
+  print(filenames)
+  dataset = tf.data.Dataset.list_files(filenames).interleave(
+      lambda filepath: tf.data.TFRecordDataset(filepath), cycle_length=2,)
+  dataset = dataset.map(decode, num_parallel_calls=2)
+  dataset = dataset.filter(lambda x: tf.reduce_any(tf.math.is_nan(x['feature/image/avg'])) == False)
+  dataset = dataset.map(lambda x: separate_features_and_labels(x))
+  dataset = dataset.repeat(repeat)
+  dataset = dataset.shuffle(2048)
+  dataset = dataset.batch(batch_size).prefetch(prefetch_size)
+  return dataset
