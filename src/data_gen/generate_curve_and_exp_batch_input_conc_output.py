39766c03ae3808f73e3da54aa25783c0d3d35a6d
Na Dai <daina.lehigh207@gmail.com>
2023-04-06 20:22:35 -0400

    update

--- src/data_gen/generate_curve_and_exp_batch_input_conc_output.py
+++ src/data_gen/generate_curve_and_exp_batch_input_conc_output.py
@@ -19,18 +19,26 @@ def separate_features_and_labels(features: Dict, label_columns: list) -> Dict:
   return (tf.keras.applications.mobilenet_v2.preprocess_input(features['feature/image/avg']), one_hot_encoder(features['metadata/batch_id'])), tf.concat(axis=-1,
               values=[features[label_columns[0]], features[label_columns[1]], features[label_columns[2]], features[label_columns[3]]])
 
+def separate_features_and_labels_one_hot_batch_id_as_labels(features: Dict, label_columns: list) -> Dict:
+  return tf.keras.applications.mobilenet_v2.preprocess_input(features['feature/image/avg']), (one_hot_encoder(features['metadata/batch_id']), tf.concat(axis=-1,
+              values=[features[label_columns[0]], features[label_columns[1]], features[label_columns[2]], features[label_columns[3]]]))
+
 def load_dataset(filename_pattern: Text, 
                  label_columns: list, 
                  batch_size: int, 
                  prefetch_size: int, 
-                 repeat: Optional[int] = None):
+                 repeat: Optional[int] = None,
+                 one_hot_batch_id_as_label: bool = False):
   filenames = [filename_pattern] if '*' not in filename_pattern else glob.glob(filename_pattern)
   print(filenames)
   dataset = tf.data.Dataset.list_files(filenames).interleave(
       lambda filepath: tf.data.TFRecordDataset(filepath), cycle_length=2,)
   dataset = dataset.map(generate_curve_input_conc_output.decode, num_parallel_calls=2)
   dataset = dataset.filter(lambda x: tf.reduce_any(tf.math.is_nan(x['feature/image/avg'])) == False)
-  dataset = dataset.map(lambda x: separate_features_and_labels(x, label_columns))
+  if not one_hot_batch_id_as_label:
+    dataset = dataset.map(lambda x: separate_features_and_labels(x, label_columns))
+  else:
+    dataset = dataset.map(lambda x: separate_features_and_labels_one_hot_batch_id_as_labels(x, label_columns))
   dataset = dataset.repeat(repeat)
   dataset = dataset.shuffle(2048)
   dataset = dataset.batch(batch_size).prefetch(prefetch_size)
