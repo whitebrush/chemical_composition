fc8291c3be063ee4352ee4376a1fa68ea6f37c5e
Na Dai <daina.lehigh207@gmail.com>
2023-04-15 15:24:18 -0400

    asdf

--- src/data_gen/generate_curve_input_conc_and_predicted_batch_output.py
+++ src/data_gen/generate_curve_input_conc_and_predicted_batch_output.py
@@ -13,12 +13,10 @@ class CurveInputConcAndPredictedBatchOutputGenerator:
 
   def separate_features_and_labels(self, features: Dict, label_columns: list) -> Dict:
     input_features = tf.keras.applications.mobilenet_v2.preprocess_input(features['feature/image/avg'])
-    print(self.batch_id_model(input_features).shape)
     return input_features, tf.concat(
       axis=-1,values=[features[label_columns[0]], features[label_columns[1]], features[label_columns[2]], features[label_columns[3]]])
 
-  def add_predicted_batch_id(self, data_set):
-    image_features, conc_labels = data_set
+  def add_predicted_batch_id(self, image_features, conc_labels):
     return image_features, (self.batch_id_model(image_features), conc_labels)
 
 
@@ -29,7 +27,6 @@ class CurveInputConcAndPredictedBatchOutputGenerator:
                    prefetch_size: int, 
                    repeat: Optional[int] = None):
     filenames = [filename_pattern] if '*' not in filename_pattern else glob.glob(filename_pattern)
-    print(filenames)
     dataset = tf.data.Dataset.list_files(filenames).interleave(
         lambda filepath: tf.data.TFRecordDataset(filepath), cycle_length=2,)
     dataset = dataset.map(generate_curve_input_conc_output.decode, num_parallel_calls=2)
