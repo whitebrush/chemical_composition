9d8c827de88cadf3ce90403235fbbe206ae3d52e
Na Dai <daina.lehigh207@gmail.com>
2023-08-16 21:40:53 -0400

    test

--- src/data_gen/generate_curve_and_exp_batch_input_conc_output.py
+++ src/data_gen/generate_curve_and_exp_batch_input_conc_output.py
@@ -7,6 +7,7 @@ from src.data_gen import generate_curve_input_conc_output
 
 def one_hot_encoder_from_numpy(batch_id_numpy):
   encoded_batch = np.array([0.0] * 17)
+  print("batch_id_number: int(batch_id_numpy[0])", int(batch_id_numpy[0]))
   encoded_batch[int(batch_id_numpy[0])] = 1.0
   return encoded_batch
 
